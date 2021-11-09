cafd7ada841b18315ee739d9788cec96f38a7222
Gudongyangg <1085503343@qq.com>
2021-11-09 13:59:14 +0800

    test中使用safetestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,16 @@
 using MyPkg
-using Test
+using SafeTestsets
+# using Test
 using QuadGK
 integral, err = quadgk(MyPkg.g,0,1,rtol=1e-5)
 
-@testset "MyPkg.jl" begin
-    # Write your tests here.
+@safetestset BenchmarkTests = "Benchmark Tests" begin
     @test Simpson(MyPkg.g,0,1) == integral
     @test Simpson(MyPkg.g,1,2) == 3
-
 end
+# @testset "MyPkg.jl" begin
+#     # Write your tests here.
+#     @test Simpson(MyPkg.g,0,1) == integral
+#     @test Simpson(MyPkg.g,1,2) == 3
+#
+# end
